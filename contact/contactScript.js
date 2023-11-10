85a8c9384d4278864cd39643757c9e1036b755bf
Philipp <philippalexanderlange@gmail.com>
2023-11-10 22:21:53 +0000

    Pl continued on form validation and incorrect input highlighting

--- contact/contactScript.js
+++ contact/contactScript.js
@@ -11,29 +11,21 @@ const phoneNumber = document.getElementById('phone-number');
 const contactReason = document.getElementById('contact-reason');
 const message = document.getElementById('message');
 
+var submitButtonHasBeenPressed = false;
+
 //source: https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
 const validateEmail = (email) => {
     if (String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-        document.getElementById('email').classList.remove('invalid-input');
         return true;
     } else {
-        if (email.value != '') {
-            document.getElementById('email').classList.add('invalid-input');
-            document.getElementById('email').placeholder = '';
-        }
         return false;
     }
 }
 
 const validatePhoneNumber = (number) => {
     if (String(number).toLowerCase().match(/^08[35679]\d{7}$|^0[1245679]\d{6,9}$/)) {
-        document.getElementById('phone-number').classList.remove('invalid-input');
         return true;
     } else {
-        if (phoneNumber.value) {
-            document.getElementById('phone-number').classList.add('invalid-input');
-            document.getElementById('phone-number').placeholder = '';
-        }
         return false;
     }
 }
@@ -54,6 +46,40 @@ const validateMessage = (message) => {
     }
 }
 
+function highlightInvalidInputs() {
+    submitButtonHasBeenPressed = true;
+    if (validateEmail(email.value)) {
+        document.getElementById('email').classList.remove('invalid-input');
+    } else {
+        document.getElementById('email').classList.add('invalid-input');
+        document.getElementById('email').placeholder = '';
+    }
+
+    if (validatePhoneNumber(phoneNumber.value)) {
+        document.getElementById('phone-number').classList.remove('invalid-input');
+    } else {
+        document.getElementById('phone-number').classList.add('invalid-input');
+        document.getElementById('phone-number').placeholder = '';
+    }
+
+    if (validateContactReason(contactReason.value)) {
+        document.getElementById('contact-reason').classList.remove('invalid-input');
+    } else {
+        document.getElementById('contact-reason').classList.add('invalid-input');
+    }
+
+    if (validateMessage(message.value)) {
+        document.getElementById('message').classList.remove('invalid-input');
+    } else {
+        document.getElementById('message').classList.add('invalid-input');
+    }
+
+}
+
+
+
+
+
 function checkButtonActivation() {
     let emailIsValid = validateEmail(email.value);
     let phoneIsValid = validatePhoneNumber(phoneNumber.value);
@@ -62,9 +88,15 @@ function checkButtonActivation() {
 
 
     if (emailIsValid && phoneIsValid && contactReasonIsValid && messageIsValid) {
-        submitButton.disabled = false;
+        document.getElementById('submit-button').classList.remove('form-button-disabled');
+        document.getElementById('submit-button').classList.add('form-button');
     } else {
-        submitButton.disabled = true;
+        document.getElementById('submit-button').classList.remove('form-button');
+        document.getElementById('submit-button').classList.add('form-button-disabled');
+    }
+
+    if (submitButtonHasBeenPressed) {
+        highlightInvalidInputs();
     }
 }
 
@@ -72,3 +104,4 @@ email.addEventListener('change', (event) => checkButtonActivation());
 phoneNumber.addEventListener('change', (event) => checkButtonActivation());
 contactReason.addEventListener('change', (event) => checkButtonActivation());
 message.addEventListener('change', (event) => checkButtonActivation());
+submitButton.addEventListener('click', (event) => highlightInvalidInputs());
