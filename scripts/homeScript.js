cddead30fbc94c3d586d1feaf0a180ef8a797fd9
Cat-McIlroy <148088736+Cat-McIlroy@users.noreply.github.com>
2023-11-13 00:08:58 +0000

    Update by CMc
    Removal of unnecessary lines of code in CSS stylesheet.
    homeScript.js commented out until I can figure out how to get it to work.
    Pathing added where possible.
    Divs added to index.html for JS minigame, Christmas flowers, all flowers, and gifts.
    Christmas animated border and themed header. Google font used  in header 2s

--- scripts/homeScript.js
+++ scripts/homeScript.js
@@ -1,13 +1,13 @@
-function dropdownMenu(){
-    var dropdown= document.getElementById("dropdown_click");
-        if(dropdown.className=== "topnav"){
-        dropdown.className+= " mobile";
-        }
-        else{
-        dropdown.className= "topnav";
-        }
-}
+// function dropdownMenu(){
+//     var dropdown= document.getElementById("dropdown_click");
+//         if(dropdown.className=== "topnav"){
+//         dropdown.className+= " mobile";
+//         }
+//         else{
+//         dropdown.className= "topnav";
+//         }
+// }
 
-document.getElementById("dropdown_icon").addEventListener("click", dropdownMenu(){
-event.preventDefault;
-});
+// document.getElementById("dropdown_icon").addEventListener("click", dropdownMenu(){
+// event.preventDefault;
+// });
