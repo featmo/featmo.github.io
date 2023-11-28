fac0f16d099dbabe3b47ac7aa50ee7e5979d1a63
Cat-McIlroy <148088736+Cat-McIlroy@users.noreply.github.com>
2023-11-28 13:55:18 +0000

    Update by CMc
    Homepage buttons updated to use JavaScript to link to pages instead of anchor tags.
    jQuery for modals condensed to use for loop

---
+++ scripts/homeScript.js
@@ -0,0 +1,19 @@
+//////////// Written by Catherine /////////////////
+
+///////////// JavaScript to control button links ////////////
+
+document.getElementById("quiz-link").addEventListener("click", function (e) {
+        location.href= "quiz/quiz.html";
+    });
+
+document.getElementById("christmas-link").addEventListener("click", function (e) {
+        location.href= "christmas/christmas.html";
+    });
+
+document.getElementById("flowers-link").addEventListener("click", function (e) {
+        location.href= "flowers/flowers.html";
+    });
+
+document.getElementById("gifts-link").addEventListener("click", function (e) {
+        location.href= "gifts/gifts.html";
+    });
--- scripts/productsScript.js
+++ scripts/productsScript.js
@@ -2,82 +2,43 @@
 
 ////// jQuery to control modal popups //////////////
 
+
 $(document).ready(function(){
         $(".first-product").click(function(){
             $("#myModal").modal('show');
         });
-    });
-
-$(document).ready(function(){
+    
         $(".second-product").click(function(){
             $("#myModal_two").modal('show');
         });
-    });
 
-$(document).ready(function(){
         $(".third-product").click(function(){
             $("#myModal_three").modal('show');
         });
-    });
 
-$(document).ready(function(){
         $(".fourth-product").click(function(){
             $("#myModal_four").modal('show');
         });
-    });
 
-$(document).ready(function(){
         $(".fifth-product").click(function(){
             $("#myModal_five").modal('show');
         });
-    });
 
-$(document).ready(function(){
         $(".sixth-product").click(function(){
             $("#myModal_six").modal('show');
         });
-    });
 
-$(document).ready(function(){
         $(".seventh-product").click(function(){
             $("#myModal_seven").modal('show');
         });
-    });
-
-$(document).ready(function(){
+   
         $(".eighth-product").click(function(){
             $("#myModal_eight").modal('show');
         });
     });
 
-document.getElementById("orderbtn-one").addEventListener("click", function(){
-alert("This is where we would integrate the checkout flow of our chosen payment provider.");
-});
-
-document.getElementById("orderbtn-two").addEventListener("click", function(){
+for(i=1;i<=8;i++){
+    document.getElementById("orderbtn-"+i).addEventListener("click", function(){
     alert("This is where we would integrate the checkout flow of our chosen payment provider.");
-});
-
-document.getElementById("orderbtn-three").addEventListener("click", function(){
-    alert("This is where we would integrate the checkout flow of our chosen payment provider.");
-});
-
-document.getElementById("orderbtn-four").addEventListener("click", function(){
-    alert("This is where we would integrate the checkout flow of our chosen payment provider.");
-});
-
-document.getElementById("orderbtn-five").addEventListener("click", function(){
-    alert("This is where we would integrate the checkout flow of our chosen payment provider.");
-});
-
-document.getElementById("orderbtn-six").addEventListener("click", function(){
-    alert("This is where we would integrate the checkout flow of our chosen payment provider.");
-});
-
-document.getElementById("orderbtn-seven").addEventListener("click", function(){
-    alert("This is where we would integrate the checkout flow of our chosen payment provider.");
-});
-
-document.getElementById("orderbtn-eight").addEventListener("click", function(){
-    alert("This is where we would integrate the checkout flow of our chosen payment provider.");
-});
+    });
+}
