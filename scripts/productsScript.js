13a1af56e920044a117fb2f20d3aeeca6528e6d8
Philipp <philippalexanderlange@gmail.com>
2023-11-29 18:34:58 +0000

    PL fixed HTML issues based on validator feedback.

--- scripts/productsScript.js
+++ scripts/productsScript.js
@@ -3,42 +3,42 @@
 ////// jQuery to control modal popups //////////////
 
 
-$(document).ready(function(){
-        $(".first-product").click(function(){
-            $("#myModal").modal('show');
-        });
-    
-        $(".second-product").click(function(){
-            $("#myModal_two").modal('show');
-        });
-
-        $(".third-product").click(function(){
-            $("#myModal_three").modal('show');
-        });
-
-        $(".fourth-product").click(function(){
-            $("#myModal_four").modal('show');
-        });
-
-        $(".fifth-product").click(function(){
-            $("#myModal_five").modal('show');
-        });
-
-        $(".sixth-product").click(function(){
-            $("#myModal_six").modal('show');
-        });
-
-        $(".seventh-product").click(function(){
-            $("#myModal_seven").modal('show');
-        });
-   
-        $(".eighth-product").click(function(){
-            $("#myModal_eight").modal('show');
-        });
+$(document).ready(function () {
+    $(".first-product").click(function () {
+        $("#myModal").modal('show');
     });
 
-for(i=1;i<=8;i++){
-    document.getElementById("orderbtn-"+i).addEventListener("click", function(){
-    alert("This is where we would integrate the checkout flow of our chosen payment provider.");
+    $(".second-product").click(function () {
+        $("#myModal_two").modal('show');
+    });
+
+    $(".third-product").click(function () {
+        $("#myModal_three").modal('show');
+    });
+
+    $(".fourth-product").click(function () {
+        $("#myModal_four").modal('show');
+    });
+
+    $(".fifth-product").click(function () {
+        $("#myModal_five").modal('show');
+    });
+
+    $(".sixth-product").click(function () {
+        $("#myModal_six").modal('show');
+    });
+
+    $(".seventh-product").click(function () {
+        $("#myModal_seven").modal('show');
+    });
+
+    $(".eighth-product").click(function () {
+        $("#myModal_eight").modal('show');
+    });
+});
+
+for (i = 1; i <= 8; i++) {
+    document.getElementById("orderbtn-" + i).addEventListener("click", function () {
+        alert("This is where we would integrate the checkout flow of our chosen payment provider.");
     });
 }
--- scripts/quizScript.js
+++ scripts/quizScript.js
@@ -11,83 +11,83 @@ let quizAnswer = "";
 let correct = []; //novel approach to holding correct guesses, values can be arbitrary
 
 //random int from 0 to max;
-function randomInt(max){
-    return Math.floor(Math.random()*(max) ); //exclusive random
+function randomInt(max) {
+    return Math.floor(Math.random() * (max)); //exclusive random
 }
 
 // simple key generation
-function generateKey(){
+function generateKey() {
     let seq = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     let key = ["TFC_"];
-    for(let i = 0; i < 10; i++){
+    for (let i = 0; i < 10; i++) {
         key.push(seq[randomInt(seq.length)]);
-    }   
+    }
     return key.join('');
 }
 
-function generateMessage(){
+function generateMessage() {
     let quiz = document.getElementById("quiz_body");
-    let win = document.getElementById("win");   
+    let win = document.getElementById("win");
     let loss = document.getElementById("loss");
     let key = document.getElementById("key");
 
-    if(correct.length > 3){
+    if (correct.length > 3) {
         win.style.display = "initial";
         key.textContent = generateKey();
     }
-    else{
-        loss.style.display = "initial"; 
-    }   
+    else {
+        loss.style.display = "initial";
+    }
     quiz.remove();
 }
 
 //generate image
-function generateImage(_currentImage_){
+function generateImage(_currentImage_) {
     randInt = randomInt(imageArray.length);
     image = imageArray[randInt];
-    imagePath  = flowerImages + image+ ".jpg";
-     _currentImage_.src = imagePath; 
-    let index = imageArray.indexOf( image );  
-    console.log("Image name: "+imageArray[index]);
-    //console.log("Length: "+imageArray.length); 
+    imagePath = flowerImages + image + ".jpg";
+    _currentImage_.src = imagePath;
+    let index = imageArray.indexOf(image);
+    console.log("Image name: " + imageArray[index]);
+    //console.log("Length: "+imageArray.length);
 }
 
 // https://stackoverflow.com/questions/9709209/html-select-only-one-checkbox-in-a-group
-function oneSelect(answer){
+function oneSelect(answer) {
     /**
      * iterate through checkbox answers
      * uncheck all but one
-     */  
+     */
     answerList.forEach((item) => {
-        if(item !== answer)
+        if (item !== answer)
             item.checked = false;
-            quizAnswer = answer.value;
-            console.log("Selected answer: "+ answer.value)
+        quizAnswer = answer.value;
+        console.log("Selected answer: " + answer.value)
     });
 
 }
 
-function checkAnswer(){
+function checkAnswer() {
     $("#myModal .modal-body").text("Incorrect!");
     answerList.forEach((item) => {
-        if(item.checked == true){          
-            if(imageArray.length > 0){
-                if(image.includes(item.value)){
+        if (item.checked == true) {
+            if (imageArray.length > 0) {
+                if (image.includes(item.value)) {
                     $("#myModal .modal-body").text("Correct! the answer was " + item.value);
-                    correct.push(1);           
-                }              
-                else{        
-                    $("#myModal .modal-body").text("Incorrect! the answer was " + image);    
+                    correct.push(1);
+                }
+                else {
+                    $("#myModal .modal-body").text("Incorrect! the answer was " + image);
                 }
             }
-            else{
+            else {
                 generateMessage();
-            }    
-        }     
+            }
+        }
         item.checked = false;
-           
+
     });
-    
+
 }
 
 // //clipboard
@@ -96,33 +96,33 @@ function checkAnswer(){
 //     //alert("Copied "+key.textContent);
 // }
 //reload page
-function reloadPage(){
+function reloadPage() {
     location.reload();
 }
 
 //show answer
-$(document).ready(function(){
-    $("#submit").click(function(){
-        $("#myModal").modal('show');      
+$(document).ready(function () {
+    $("#submit").click(function () {
+        $("#myModal").modal('show');
         checkAnswer();
         imageArray.splice(imageArray.indexOf(image), 1);
         generateImage(quizImage);
-            
+
     });
-    
+
 });
 
 //to clip board
-$(document).ready(function(){
-    $("#key").click(function(){
-        navigator.clipboard.writeText( $("#key").text() );
-        $("#clip-response").modal('show');   
-        
+$(document).ready(function () {
+    $("#key").click(function () {
+        navigator.clipboard.writeText($("#key").text());
+        $("#clip-response").modal('show');
+
     });
 });
 
 //img src undefined error quick fix
-$("#quiz_image").on("error", function (){
+$("#quiz_image").on("error", function () {
     generateMessage();
 });
 
